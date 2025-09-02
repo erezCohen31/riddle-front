be21cf044f515409f2ccb3dee24ecabad7e65c01
erez cohen <erezjbh@gmail.com>
2025-09-02 17:55:59 +0300

    add the pages

---
+++ src/pages/CreateRiddlePage.tsx
@@ -0,0 +1,3 @@
+export default function CreateRiddlePage() {
+  return <div>CreateRiddlePage</div>;
+}
---
+++ src/pages/DeleteRiddlePage.tsx
@@ -0,0 +1,3 @@
+export default function DeleteRiddlePage() {
+  return <div>DeleteRiddlePage</div>;
+}
