bf6f1d33d3fc0ec92368e4a07b326b57ce885722
erez cohen <erezjbh@gmail.com>
2025-09-02 17:21:14 +0300

    add main menu

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,25 @@ import "./App.css";
 import ConnectPage from "./pages/ConnectPage";
 import { BrowserRouter, Routes, Route } from "react-router";
 import MainMenuPage from "./pages/MainMenuPage.tsx";
+import { RoleProvider } from "./contexts/Role.context.tsx";
+import PlayPage from "./pages/PlayPage.tsx";
+import ScorePage from "./pages/ScorePage.tsx";
+import ModifyPlayerPage from "./pages/ModifyPlayerPage.tsx";
+import ModifyRiddlePage from "./pages/ModifyRiddlePage.tsx";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<ConnectPage />} />
-        <Route path="/main-menu" element={<MainMenuPage />} />
-      </Routes>
+      <RoleProvider>
+        <Routes>
+          <Route path="/" element={<ConnectPage />} />
+          <Route path="/main-menu" element={<MainMenuPage />} />
+          <Route path="/play" element={<PlayPage />} />
+          <Route path="/score" element={<ScorePage />} />
+          <Route path="/manage-riddles" element={<ModifyRiddlePage />} />
+          <Route path="/manage-players" element={<ModifyPlayerPage />} />
+        </Routes>
+      </RoleProvider>
     </BrowserRouter>
   );
 }
--- src/pages/MainMenuPage.tsx
+++ src/pages/MainMenuPage.tsx
@@ -1,3 +1,37 @@
+import { useContext } from "react";
+import { RoleContext } from "../contexts/Role.context";
+import { useLocation, useNavigate } from "react-router";
+
 export default function MainMenuPage() {
-  return <div>MainMenuPage</div>;
+  const context = useContext(RoleContext);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { username } = location.state;
+
+  const { role } = context;
+
+  function handleSubmit(destination: string) {
+    navigate(`/${destination}`);
+  }
+
+  return (
+    <>
+      <div>MainMenuPage</div>
+      <h2>Welcome {username}</h2>
+      <button onClick={() => handleSubmit("play")}>To Play</button>
+      <button onClick={() => handleSubmit("score")}>Scores</button>
+
+      {(role === "user" || role === "admin") && (
+        <button onClick={() => handleSubmit("manage-riddles")}>
+          Manage Riddles
+        </button>
+      )}
+
+      {role === "admin" && (
+        <button onClick={() => handleSubmit("manage-players")}>
+          Manage Players
+        </button>
+      )}
+    </>
+  );
 }
